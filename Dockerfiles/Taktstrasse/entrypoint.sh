ecacf12012f649aaba96c4fadbc5c41101b07f72
AlexDeckert <alexander.deckert1995@gmail.com>
2016-11-11 14:48:08 +0100

    Meteor Ordner und Dockerfile angelegt und einige Anpassungen in Taktstrasse

--- Dockerfiles/Taktstrasse/entrypoint.sh
+++ Dockerfiles/Taktstrasse/entrypoint.sh
@@ -1,7 +1,10 @@
 !/bin/bash
 
 # start simulation
-java -jar TaktstrasseOpcServer-0.0.1-SNAPSHOT.jar -o /Data -amqp tcp://activemq:61616 -d 10000 -kafka kafka:9092 -topic prodData &
+java -jar TaktstrasseOpcServer-0.0.1-SNAPSHOT.jar -o /Data -amqp tcp://activemq:61616 -d 5000 -kafka kafka:9092 -topic prodData &
+
+# start consumer
+#java -jar consumer.jar &
 
 # run endless loop to keep alive
 /usr/bin/tail -f /dev/null
