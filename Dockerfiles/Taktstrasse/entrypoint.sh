39612e67762b40754a1caf5f8dbd2824cb5b0e1a
AlexDeckert <alexander.deckert1995@gmail.com>
2016-11-13 00:14:28 +0100

    Consumer laeuft im Docker-Compose aber nicht die komplette Datenuebertragung

--- Dockerfiles/Taktstrasse/entrypoint.sh
+++ Dockerfiles/Taktstrasse/entrypoint.sh
@@ -1,10 +1,10 @@
 !/bin/bash
 
-# start simulation
-java -jar TaktstrasseOpcServer-0.0.1-SNAPSHOT.jar -o /Data -amqp tcp://activemq:61616 -d 5000 -kafka kafka:9092 -topic prodData &
-
 # start consumer
-#java -jar consumer.jar &
+java -jar consumer.jar &
+
+# start simulation
+java -jar TaktstrasseOpcServer-0.0.1-SNAPSHOT.jar -o /Data -amqp tcp://activemq:61616 -d 10000 -kafka kafka:9092 -topic prodData &
 
 # run endless loop to keep alive
 /usr/bin/tail -f /dev/null
